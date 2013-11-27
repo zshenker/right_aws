e13e3585c632b5a3045d14e3975cf2a5cd679a80
Zac Shenker <zshenker@brightcove.com>
2013-11-27 09:58:21 -0800

    Change version number to 3.1.0.zencoder.1

--- lib/awsbase/version.rb
+++ lib/awsbase/version.rb
@@ -2,8 +2,10 @@ module RightAws #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 3  unless defined?(MAJOR)
     MINOR = 1  unless defined?(MINOR)
-    TINY  = 1  unless defined?(TINY)
+    TINY  = 0  unless defined?(TINY)
+    VENDOR = 'zencoder' unless defined?(VENDOR)
+    VENDOR_NUM = 1 unless defined?(VENDOR_NUM)
 
-    STRING = [MAJOR, MINOR, TINY].join('.') unless defined?(STRING)
+    STRING = [MAJOR, MINOR, TINY, VENDOR, VENDOR_NUM].join('.') unless defined?(STRING)
   end
 end
